95546f198883341361011316580224c860e18b5d
yohelab2026 <yohelab2026@gmail.com>
2026-05-02 00:59:05 +0900

    Commit generated function assets

---
+++ functions/generated/theme-update.js
@@ -0,0 +1,1 @@
+export const themeUpdateJson = "{\n  \"version\": \"0.1.3\",\n  \"details_url\": \"https://yohelab.com/lp/bunsirube/\",\n  \"package\": \"https://yohelab.com/api/theme-download\"\n}\n";
