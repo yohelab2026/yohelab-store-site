3d1f86e0c07defdbd8d11635fceadaa9d02b2d3a
yohelab2026 <yohelab2026@gmail.com>
2026-05-04 12:08:10 +0900

    Release Bunsirube 0.3.1 package

--- functions/api/theme-download.js
+++ functions/api/theme-download.js
@@ -3,8 +3,8 @@ import { fetchStripePurchaseStatus } from "../lib/stripe-purchase.js";
 
 const THEME_FILES = {
   parent: {
-    key: "bunsirube-0.3.0.zip",
-    filename: "bunsirube-0.3.0.zip",
+    key: "bunsirube-0.3.1.zip",
+    filename: "bunsirube-0.3.1.zip",
   },
   child: {
     key: "bunsirube-child-0.1.1.zip",
--- functions/generated/theme-update.js
+++ functions/generated/theme-update.js
@@ -1,1 +1,1 @@
-export const themeUpdateJson = "{\n  \"version\": \"0.3.0\",\n  \"details_url\": \"https://yohelab.com/lp/bunsirube/\",\n  \"package\": \"https://yohelab.com/api/theme-download\"\n}\n";
+export const themeUpdateJson = "{\n  \"version\": \"0.3.1\",\n  \"details_url\": \"https://yohelab.com/lp/bunsirube/\",\n  \"package\": \"https://yohelab.com/api/theme-download\"\n}\n";
